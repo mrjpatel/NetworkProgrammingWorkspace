11261890b0cad84196cd60a27ab52398c58a5e54
Japan Patel <japanpatel1198@gmail.com>
2019-04-02 15:44:20 +1100

    Made client socket port configurable

--- src/clientServerSocket/Client.java
+++ src/clientServerSocket/Client.java
@@ -1,6 +1,8 @@
 package clientServerSocket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
@@ -11,22 +13,44 @@ public class Client {
 	
 	private Socket clientSocket;
 	
+	private BufferedReader bufferReader;
+	
 	public static void main(String args[]) 
     { 
         Client client = new Client("localhost", 61027); 
+        client.getServerAddress();
         client.connectToServer();
-    } 
+    }
 	
 	public Client(String address, int port) {
 		this.address = address;
 		this.port = port;
 	}
 	
+	public void getServerAddress() {
+		System.out.print("Please input the server address (e.g. 127.0.0.1:61027) - ");
+		BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in)); 
+		try {
+			String userInput = reader.readLine();
+			this.address = userInput.split(":")[0];
+			this.port = Integer.parseInt(userInput.split(":")[1]);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void connectToServer(){
 		try {
-			System.out.println("Attempting to connect to server");
+			System.out.println("Attempting to connect to server on: " + this.address + ":" + this.port);
 			clientSocket = new Socket(address, port);
-			System.out.println("Server Connected!");
+			System.out.println("Server Connected! Waiting for a message...");
+			
+			bufferReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			String message = bufferReader.readLine();
+			System.out.println("Message received from server: " + message);
+			
+			closeConnection();
 		} 
 		catch (IOException e) {
 			e.printStackTrace();
@@ -34,8 +58,9 @@ public class Client {
     }
 	
 	public void closeConnection() {
-		System.out.println("Closing connection");
+		System.out.println("Closing connection...");
 		try {
+			bufferReader.close();
 			clientSocket.close();
 		} 
 		catch (IOException e) {
