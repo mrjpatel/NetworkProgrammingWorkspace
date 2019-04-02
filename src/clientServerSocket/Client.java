920d27c536470bc96a37ba47dec659f47f98cc69
Japan Patel <japanpatel1198@gmail.com>
2019-04-02 16:08:09 +1100

    Added comments and made code readable

--- src/clientServerSocket/Client.java
+++ src/clientServerSocket/Client.java
@@ -5,28 +5,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+/**
+ * A TCP server that connects to the
+ * server and receives, data from the
+ * server and closes the connection.
+ * @author Japan Patel
+ *
+ */
 public class Client {
 
+	/**
+	 * The server Address
+	 */
 	private String address;
 	
+	/**
+	 * The port number that server is running
+	 */
 	private int port;
 	
+	/**
+	 * Client Socket that connects to server
+	 */
 	private Socket clientSocket;
 	
+	/**
+	 * Buffered Reader reads user input
+	 */
 	private BufferedReader bufferReader;
 	
+	/**
+	 * Runs the server
+	 * @param args None.
+	 */
 	public static void main(String args[]) 
     { 
         Client client = new Client("localhost", 61027); 
         client.getServerAddress();
         client.connectToServer();
+        client.closeConnection();
     }
 	
+	/**
+	 * Initialises the server address and port
+	 * @param address The server address
+	 * @param port The server port number
+	 */
 	public Client(String address, int port) {
 		this.address = address;
 		this.port = port;
 	}
 	
+	/**
+	 * Reads User input for a desired server address
+	 * and the port numbers
+	 */
 	public void getServerAddress() {
 		System.out.print("Please input the server address (e.g. 127.0.0.1:61027) - ");
 		BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in)); 
@@ -40,6 +73,10 @@ public class Client {
 		}
 	}
 	
+	/**
+	 * Connects to the Server and
+	 * Reads the data sent from the server.
+	 */
 	public void connectToServer(){
 		try {
 			System.out.println("Attempting to connect to server on: " + this.address + ":" + this.port);
@@ -49,14 +86,15 @@ public class Client {
 			bufferReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			String message = bufferReader.readLine();
 			System.out.println("Message received from server: " + message);
-			
-			closeConnection();
 		} 
 		catch (IOException e) {
 			e.printStackTrace();
 		}
     }
 	
+	/**
+	 * Closes the connection
+	 */
 	public void closeConnection() {
 		System.out.println("Closing connection...");
 		try {
--- src/clientServerSocket/Server.java
+++ src/clientServerSocket/Server.java
@@ -6,25 +6,58 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 
+/**
+ * A TCP server that runs on user desired port
+ * When a client connects, it sends the
+ * client the current date and time, then
+ * closes the connection with that client.
+ * 
+ * @author Japan Patel
+ */
 public class Server {
 	
+	/**
+	 * Port number
+	 */
 	private int port;
 	
+	/**
+	 * Server Socket
+	 */
 	private ServerSocket serverSocket;
 	
+	/**
+	 * Socket that accepts client connection
+	 */
 	private Socket socket;
 	
+	/**
+	 * Writes data on a given stream
+	 */
 	private PrintWriter printWriter;
 	
+	/**
+	 * Runs the server
+	 * @param args The desired port number.
+	 */
 	public static void main(String[] args) {		
 		Server server = new Server(Integer.parseInt(args[0]));
 		server.connectToClient();
+		server.closeConnection();
 	}
 	
+	/**
+	 * Initialises Port number
+	 * @param port The desired port number
+	 */
 	public Server(int port) { 
 		this.port = port;
 	}
 	
+	/**
+	 * Connects to the client
+	 * and sends the date.
+	 */
 	public void connectToClient() {
 		try {
 			serverSocket = new ServerSocket(port);
@@ -37,13 +70,15 @@ public class Server {
 			printWriter.println(new Date().toString());
 			System.out.println("Successfully sent data to client.");
 			
-			closeConnection();
 		} 
 		catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("IO exception when connecting to client. " + e);
 		}
 	}
 	
+	/**
+	 * closes the connection
+	 */
 	public void closeConnection() {
 		System.out.println("Closing connection now...");
 		try {
@@ -52,7 +87,7 @@ public class Server {
 			serverSocket.close();
 		} 
 		catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("IO exception when closing the socket. " + e);
 		}
 	}
 }
