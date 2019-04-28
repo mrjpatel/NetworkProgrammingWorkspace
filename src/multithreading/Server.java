92ad6c54fa9333574146f982e2794bf68d9d8f36
Japan Patel <japanpatel1198@gmail.com>
2019-04-28 21:00:28 +1000

    Created a mutithreaded server that accepts connection from mutiple clients

--- src/multithreading/Server.java
+++ src/multithreading/Server.java
@@ -8,64 +8,92 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
-	private static final String ONE_HUNDRED_MEGABYTE_FILE = "100_megabyte_file_out.txt";
+public class Server implements Runnable{
+	
+	private int port = 0;
+	
+	private ServerSocket serverSocket;
 
-    public static void main(String [] args)
-    {
-        new Server();
+    public static void main(String [] args){
+    	
+    	int port = Integer.parseInt(args[0]);
+        Server server = new Server(port);
+    	
+        Thread connectionThread = new Thread(server);
+    	connectionThread.start();
     }
+    
+    public Server(int port) {
+    	this.port = port;
+    }
+    
+    private boolean isThreadInterrupted(){
+		return Thread.currentThread().isInterrupted();
+	}
+    
+    @Override
+	public void run() {
+    	try {
+			serverSocket = new ServerSocket(port);
+			System.out.println("Started a Server Socket on port: " + serverSocket.getLocalPort());
+			
+			while(!isThreadInterrupted()){
+				Socket clientSocket = serverSocket.accept();
+				new ServerFileReceiver(clientSocket);
+			}
+			serverSocket.close();
+		}
+    	catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+    
+    private class ServerFileReceiver{
 
-    public Server()
-    {
-        System.out.println("Server running");
-        ServerSocket serverSocket = null;
-        Socket connection = null;
-        InputStream inputStream = null;
-        FileOutputStream fileOutputStream;
-        BufferedOutputStream bufferedOutputStream = null;
-
-        try {
-            serverSocket = new ServerSocket(61174); // Binds to the server port
-
-            while(true)
-            {
-                connection = serverSocket.accept(); // Create a connection between server and client
-                System.out.println("Accepted Client");
-                inputStream = connection.getInputStream(); // Access the read stream
-                byte [] buffer = new byte[1024];
-                int bytesRead;
-
-                File file = new File(ONE_HUNDRED_MEGABYTE_FILE);
-                fileOutputStream = new FileOutputStream(file);
-                bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
-
-                /* Reads and write the byte stream from the client onto the server disk */
-                do
-                {
-                    bytesRead = inputStream.read(buffer, 0, buffer.length);
-
-                    if(bytesRead <= 0)
-                        break;
-
-                    bufferedOutputStream.write(buffer, 0, bytesRead);
-                    bufferedOutputStream.flush();
-                }
-                while (true);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally
-        {
-            try {
-                bufferedOutputStream.close();
-                inputStream.close();
-                connection.close();
-                serverSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    	private Socket clientSocket;
+	    private InputStream inputStream = null;
+	    private FileOutputStream fileOutputStream;
+	    private BufferedOutputStream bufferedOutputStream = null;
+	    private String ONE_HUNDRED_MEGABYTE_FILE = "100_megabyte_file_out_";
+    	
+		public ServerFileReceiver(Socket clientSocket) {
+			this.clientSocket = clientSocket;
+			this.readFileData();
+//			System.out.println("Inside the file receiver: " + clientSocket.getLocalPort());
+		}
+		
+		private void readFileData() {
+			try {
+				inputStream = clientSocket.getInputStream();
+				byte [] buffer = new byte[1024];
+				int bytesRead;
+				
+				File file = new File(ONE_HUNDRED_MEGABYTE_FILE + clientSocket.getPort() + ".txt");
+				fileOutputStream = new FileOutputStream(file);
+				bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+				
+				do {
+					bytesRead = inputStream.read(buffer, 0, buffer.length);
+					if(bytesRead <= 0)
+						break;
+					bufferedOutputStream.write(buffer, 0, bytesRead);
+					bufferedOutputStream.flush();
+				} while (true);
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					bufferedOutputStream.close();
+					inputStream.close();
+					clientSocket.close();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}	
     }
 }
