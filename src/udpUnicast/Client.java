9b13c1a31f0784e355c6d389f3e633bb3ace4628
Japan Patel <japanpatel1198@gmail.com>
2019-05-06 14:43:54 +1000

    Added a client and Server for unicast

--- src/udpUnicast/Client.java
+++ src/udpUnicast/Client.java
@@ -1,9 +1,36 @@
 package udpUnicast;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+/**
+ * Client responsible for connecting to Server.
+ * @author japan
+ *
+ */
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String message = "";
+		
+		try {
+			DatagramSocket socket = new DatagramSocket(62017);
+			while(true) {
+				byte[] data = new byte[1024];
+				DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+				socket.receive(receivePacket);
+				message = new String(receivePacket.getData());
+				System.out.println(message.trim());
+			}
+			
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
--- src/udpUnicast/Server.java
+++ src/udpUnicast/Server.java
@@ -1,10 +1,61 @@
 package udpUnicast;
 
-public class Server {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/**
+ * Server responsible for starting udp connection
+ * @author japan
+ *
+ */
+public class Server implements Runnable{
 
+	private int port;
+	private DatagramSocket socket;
+	
+	private Thread process;
+	
+	public Server(int port) {
+		this.port = port;
+	}
+	
+	public void initialise() {
+		try {
+			this.socket = new DatagramSocket();
+			process = new Thread(this);
+			process.start();
+		} 
+		catch (SocketException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	@Override
+	public void run() {
+		System.out.println("Server started on port " + port);
+		String message = "UDP Server. This is a message from Server.";
+		byte[] data = new byte[1024];
+		data = message.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(data, data.length, new InetSocketAddress("localhost", this.port));
+		try {
+			 while (true) {
+				 socket.send(sendPacket);
+				 Thread.sleep(3*1000);
+			 }
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static void main(String[] args) {
+		Server server = new Server(62017);
+		server.initialise();
+	}
 }
